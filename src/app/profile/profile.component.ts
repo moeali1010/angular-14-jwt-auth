c0fca976fd9c0b7837166d694e04b200ccb408c6
Mohamed Ali <workmail1010@gmail.com>
2022-09-15 14:00:00 +0200

    profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StorageService } from '../_services/storage.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  currentUser: any;
 
-  constructor() { }
+  constructor(private storageService: StorageService) {}
 
   ngOnInit(): void {
+    this.currentUser = this.storageService.getUser();
   }
-
 }
